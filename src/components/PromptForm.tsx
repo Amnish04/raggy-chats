036a6c20881e921966cb2f30b1fd929cad1e3713
Amnish Singh Arora <78865303+Amnish04@users.noreply.github.com>
2024-06-16 00:21:32 -0400

    Cancel any remaining embedding generation on exceptions (#9)

--- src/components/PromptForm.tsx
+++ src/components/PromptForm.tsx
@@ -144,6 +144,7 @@ export default function PromptForm() {
                     message: err.message,
                 });
 
+                handleClose();
                 closeToast(progressToastId);
             }
         },
